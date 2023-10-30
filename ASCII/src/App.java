77921b7c36baf5898c63ae7f6becc5929fbf2ca3
Michel Medina Alcantara <142338572+Michel-Medina@users.noreply.github.com>
2023-10-30 17:55:59 -0400

    Update ASCII.java

--- ASCII/src/App.java
+++ ASCII/src/App.java
@@ -1,5 +1,21 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class ASCII {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+
+        System.out.println("Insira um caractere do teclado: ");
+
+            char letra = leitor.next() . charAt(0);
+
+        System.out.println("Valor ASCII: "+(int)letra);
+
+        System.out.print("Valor binario: ");
+
+            for(int cont=128;cont>0;cont/=2); {
+                if(cont==8) System.out.println(" ");
+                if((((int)letra) &cont)!=0) System.out.print(1);
+                    else System.out.print(0);
+            }  
     }
 }
